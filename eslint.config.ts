7947e008188618fa7c8562adeb3c3711669be2e9
PowerNether <64768544+PowerNether@users.noreply.github.com>
2026-05-28 01:59:52 +0500

    chore: add Storybook with Vue and Vite

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,9 @@
 import antfu from '@antfu/eslint-config'
+import storybook from 'eslint-plugin-storybook'
 
 export default antfu({
   type: 'lib',
   formatters: {
     css: true,
   },
-})
+}, ...storybook.configs['flat/recommended'])
